c21e7d4834c989598bfc19939e65d0864ff7a393
Gocha <Gocha@DESKTOP-6LG1840>
2021-01-29 11:28:58 -0800

    Created domain classes

---
+++ OnlineShop.Domain/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Domain
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string ContentType { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
